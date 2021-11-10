9423e193de1f372d7c2a9a2335528a6b06a949ec
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-10 15:55:27 +0100

    prova con un bottone

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@ var root = new Vue ({
     el: '#root',
     data: {
         message: 'Hello World'
+    },
+    methods: {
+        saluta: function () {
+            return console.log("Ciao");;
+        }
     }
+
 });
